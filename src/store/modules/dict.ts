bc4eb1f2ff93abe42de5022b81c77f605d19a77d
ben1289 <ben_1289@163.com>
2024-01-23 14:54:53 +0800

    refactor(store): 字典缓存设置有效期

--- src/store/modules/dict.ts
+++ src/store/modules/dict.ts
@@ -7,24 +7,37 @@ const STORE_ID = 'dictStore'
 
 const useDictStore = defineStore(STORE_ID, () => {
   const isSet = ref(false)
+  const expires = ref<Date>()
   const dict = ref<Record<string, Dict[]>>({})
 
   async function setDict() {
     const { data } = await getDictDataSimpleList()
     dict.value = groupBy(data, 'dictType')
+    expires.value = new Date(Date.now() + 1000 * 60 * 30)
     isSet.value = true
   }
 
   function clearStorage() {
-    window.sessionStorage.removeItem(STORE_ID)
+    sessionStorage.removeItem(STORE_ID)
   }
 
   return {
     isSet,
+    expires,
     dict,
     setDict,
     clearStorage,
   }
-}, { persist: { storage: sessionStorage } })
+}, {
+  persist: {
+    storage: sessionStorage,
+    beforeRestore() {
+      const { expires } = JSON.parse(sessionStorage.getItem(STORE_ID) ?? '')
+      if (!expires || new Date(expires) < new Date()) {
+        sessionStorage.removeItem(STORE_ID)
+      }
+    },
+  },
+})
 
 export default useDictStore
