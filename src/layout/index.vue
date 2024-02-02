b0608b8c6c3310e3a3dc95fbc095a328b3d2e5b1
ben1289 <ben_1289@163.com>
2024-02-02 17:09:10 +0800

    refactor(layout): 使用useGlobImport 减少无用的加载

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,13 +1,12 @@
 <script setup lang="ts">
-import Classics from './mode/Classics.vue'
+import { useGlobImport } from '@/hooks'
 import { useAppStore } from '@/store'
 
 defineOptions({ name: 'Layout' })
 
-const layouts = import.meta.glob('./mode/*.vue', { import: 'default', eager: true })
+const layouts = import.meta.glob('./mode/*.vue', { import: 'default' })
 const appstore = useAppStore()
-const layoutKey = Object.keys(layouts).find(key => key.includes(appstore.layout.mode))
-const layout = layoutKey ? layouts[layoutKey] : Classics
+const layout = useGlobImport(layouts, appstore.layout.mode)
 </script>
 
 <template>
