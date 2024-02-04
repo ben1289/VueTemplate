f1c5f244be66d2287abc6dd6c8cf1fa4d1f69a1a
ben1289 <ben_1289@163.com>
2024-02-04 18:10:09 +0800

    refactor(hooks): 优化useGlobImport钩子
    1、path增加数组匹配
    2、新增options参数

--- src/hooks/globImport.ts
+++ src/hooks/globImport.ts
@@ -1,27 +1,59 @@
 import type { MaybeRef } from 'vue'
 import { computedAsync } from '@vueuse/core'
 
+type GlobImportOptions = {
+  eager?: false
+  needLazy?: boolean
+} | {
+  eager: true
+}
+
 /**
  * 动态导入模块
  * @param modules 模块源
  * @param path 模糊匹配模块路径
- * @param needLazy 是否需要返回懒加载的模块
+ * @param options \{ eager: 对应glob导入的eager, needLazy: 返回的模块是否为懒加载（默认否） }
  */
-export default function useGlobImport(modules: Record<string, any>, path: MaybeRef<string>, needLazy = false) {
+export default function useGlobImport(
+  modules: Record<string, any>,
+  path: MaybeRef<string | string[]>,
+  options: GlobImportOptions = { eager: false, needLazy: false },
+) {
+  const moduleKeys = Object.keys(modules)
+
   function findModuleKey() {
-    const moduleKey = Object.keys(modules).find(key => key.includes(toValue(path)))
-    if (!moduleKey) {
+    const _path = toValue(path)
+    let moduleKey: undefined | string | string[]
+    if (Array.isArray(_path)) {
+      moduleKey = moduleKeys.filter(key => key.match(new RegExp(_path.join('|'))))
+    } else {
+      moduleKey = moduleKeys.find(key => key.includes(_path))
+    }
+    if (!moduleKey?.length) {
       throw new Error('No match module found')
     }
     return moduleKey
   }
 
-  if (needLazy) {
-    return computed(() => modules[findModuleKey()])
+  if (options.eager || options?.needLazy) {
+    return computed(() => {
+      const moduleKey = findModuleKey()
+      if (Array.isArray(moduleKey)) {
+        return moduleKey.map(key => modules[key])
+      } else {
+        return modules[moduleKey]
+      }
+    })
   } else {
     return computedAsync(async () => {
       const moduleKey = findModuleKey()
-      return typeof modules[moduleKey] === 'function' ? await modules[moduleKey]() : modules[moduleKey]
+      if (Array.isArray(moduleKey)) {
+        return await Promise.all(
+          moduleKey.map(key => typeof modules[key] === 'function' ? modules[key]() : modules[key]),
+        )
+      } else {
+        return typeof modules[moduleKey] === 'function' ? await modules[moduleKey]() : modules[moduleKey]
+      }
     })
   }
 }
