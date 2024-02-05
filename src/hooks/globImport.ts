e1cf93b54c465ee88e03cfcf6af1467feb48ae39
ben1289 <ben_1289@163.com>
2024-02-05 15:19:57 +0800

    refactor(hooks): 优化useGlobImport钩子
    1、options参数增加needRaw

--- src/hooks/globImport.ts
+++ src/hooks/globImport.ts
@@ -1,59 +1,56 @@
 import type { MaybeRef } from 'vue'
 import { computedAsync } from '@vueuse/core'
 
-type GlobImportOptions = {
-  eager?: false
-  needLazy?: boolean
-} | {
-  eager: true
-}
+type GlobImportOptions = (
+    { eager?: false, needLazy?: boolean }
+    | { eager: true }
+  )
+  & { needRaw?: boolean }
 
 /**
  * 动态导入模块
  * @param modules 模块源
  * @param path 模糊匹配模块路径
- * @param options \{ eager: 对应glob导入的eager, needLazy: 返回的模块是否为懒加载（默认否） }
+ * @param options 额外参数
+ * @param options.eager 对应glob导入的eager
+ * @param options.needLazy 是否需要返回懒加载模块（默认否）
+ * @param options.needRaw 是否需要返回raw值（默认否）
  */
 export default function useGlobImport(
   modules: Record<string, any>,
   path: MaybeRef<string | string[]>,
-  options: GlobImportOptions = { eager: false, needLazy: false },
+  options: GlobImportOptions = { eager: false, needLazy: false, needRaw: false },
 ) {
   const moduleKeys = Object.keys(modules)
 
-  function findModuleKey() {
+  function findModule() {
     const _path = toValue(path)
-    let moduleKey: undefined | string | string[]
+    let module
     if (Array.isArray(_path)) {
-      moduleKey = moduleKeys.filter(key => key.match(new RegExp(_path.join('|'))))
+      module = moduleKeys.filter(key => key.match(new RegExp(_path.join('|')))).map(key => modules[key])
     } else {
-      moduleKey = moduleKeys.find(key => key.includes(_path))
+      const key = moduleKeys.find(key => key.includes(_path))
+      if (key) {
+        module = modules[key]
+      }
     }
-    if (!moduleKey?.length) {
+    if (!module || (Array.isArray(module) && module.length === 0)) {
       throw new Error('No match module found')
     }
-    return moduleKey
+    return module
   }
 
   if (options.eager || options?.needLazy) {
-    return computed(() => {
-      const moduleKey = findModuleKey()
-      if (Array.isArray(moduleKey)) {
-        return moduleKey.map(key => modules[key])
-      } else {
-        return modules[moduleKey]
-      }
-    })
+    return options.needRaw ? findModule() : computed(findModule)
   } else {
-    return computedAsync(async () => {
-      const moduleKey = findModuleKey()
-      if (Array.isArray(moduleKey)) {
-        return await Promise.all(
-          moduleKey.map(key => typeof modules[key] === 'function' ? modules[key]() : modules[key]),
-        )
+    const _fn = async () => {
+      const module = findModule()
+      if (Array.isArray(module)) {
+        return await Promise.all(module.map(mod => typeof mod === 'function' ? mod() : mod))
       } else {
-        return typeof modules[moduleKey] === 'function' ? await modules[moduleKey]() : modules[moduleKey]
+        return typeof module === 'function' ? await module() : module
       }
-    })
+    }
+    return options.needRaw ? _fn() : computedAsync(_fn)
   }
 }
