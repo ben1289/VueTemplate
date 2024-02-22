9039946e6b4b5e78d74c5782be04499c8bb3be5d
ben1289 <ben_1289@163.com>
2024-02-22 15:11:32 +0800

    refactor(enums): 新增数据权限范围字典

--- src/enums/dict.ts
+++ src/enums/dict.ts
@@ -1,11 +1,20 @@
 export enum dictTypeEnum {
   COMMON_STATUS = 'common_status',
+  DATA_SCOPE = 'system_data_scope',
   USER_SEX = 'system_user_sex',
 }
 
 export enum commonStateEnum {
   DISABLE = 0,
   ENABLE = 1,
+}
+
+export enum dataScopeEnum {
+  ALL = 1,
+  DEPT_CUSTOM = 2,
+  DEPT_ONLY = 3,
+  DEPT_AND_CHILD = 4,
+  SELF = 5,
 }
 
 export enum userSexEnum {
