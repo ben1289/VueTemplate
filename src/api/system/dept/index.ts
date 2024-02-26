d33f9d8e9257f4b16c9ee17767387ff80b33751b
ben1289 <ben_1289@163.com>
2024-02-26 13:43:38 +0800

    refactor(api): 部门管理

--- src/api/system/dept/index.ts
+++ src/api/system/dept/index.ts
@@ -10,3 +10,63 @@ export function getDeptSimpleList(): ResponseContent {
     method: 'GET',
   })
 }
+
+/**
+ * 获取部门列表
+ * @param data
+ */
+export function getDeptList(data: any): ResponseContent {
+  return service({
+    url: '/system/dept/list',
+    method: 'POST',
+    data,
+  })
+}
+
+/**
+ * 获取部门详情
+ * @param id
+ */
+export function getDeptDetail(id: number): ResponseContent {
+  return service({
+    url: '/system/dept/detail',
+    method: 'GET',
+    params: { id },
+  })
+}
+
+/**
+ * 保存部门
+ * @param data
+ */
+export function saveDept(data: any): ResponseContent {
+  return service({
+    url: '/system/dept/save',
+    method: 'POST',
+    data,
+  })
+}
+
+/**
+ * 更新部门状态
+ * @param data
+ */
+export function updateDeptState(data: any): ResponseContent {
+  return service({
+    url: '/system/dept/update-state',
+    method: 'PUT',
+    data,
+  })
+}
+
+/**
+ * 删除部门
+ * @param id
+ */
+export function deleteDept(id: number): ResponseContent {
+  return service({
+    url: '/system/dept/delete',
+    method: 'GET',
+    params: { id },
+  })
+}
