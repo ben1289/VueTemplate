10e72343b362c14a3c2e4ff05e2755fb8771b44d
ben1289 <ben_1289@163.com>
2024-03-07 18:36:17 +0800

    refactor(components): search-input 支持 Arco 表单校验

--- src/components/SearchInput/src/SearchInput.vue
+++ src/components/SearchInput/src/SearchInput.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import type { SearchInputProps } from './SearchInput'
 import type { GetFetchData } from './types'
+import { useFormItem } from '@arco-design/web-vue'
 import PopoverTable from './components/PopoverTable.vue'
 
 defineOptions({ name: 'SearchInput' })
@@ -8,34 +9,40 @@ defineOptions({ name: 'SearchInput' })
 const props = defineProps<SearchInputProps>()
 
 const emit = defineEmits<{
-  (e: 'change', value: string | number, row: Row): void
+  (e: 'change', value?: string | number, row?: Row): void
 }>()
 
 type Row = GetFetchData<typeof props.fetchData>
+
+const { eventHandlers } = useFormItem()
 
 const value = defineModel<string | number>('value', { default: '' })
 const label = defineModel<string | number>('label', { default: '' })
 const string = ref('')
 
 watch(label, () => {
-  string.value = toValue(label).toString()
+  string.value = toValue(label)?.toString()
 }, { immediate: true })
 
 const _fieldNames = computed(() => ({ valueKey: 'value', labelKey: 'label', rowKey: 'id', ...props.fieldNames }))
 
 function handleStringChange(str: string) {
   if (!str) {
-    change({})
+    change()
   } else if (str !== toValue(label)) {
-    string.value = toValue(label).toString()
+    string.value = toValue(label)?.toString()
   }
 }
 
-function change(row: Row) {
+function change(row?: Row) {
   const { valueKey, labelKey } = toValue(_fieldNames)
-  value.value = row[valueKey]
-  label.value = row[labelKey]
-  emit('change', toValue(value), row)
+  value.value = row?.[valueKey]
+  label.value = row?.[labelKey]
+
+  nextTick(() => {
+    emit('change', toValue(value), row)
+    toValue(eventHandlers)?.onChange?.()
+  })
 }
 </script>
 
