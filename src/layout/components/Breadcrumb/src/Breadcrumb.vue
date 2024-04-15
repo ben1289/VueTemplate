d476d4658cddd00a2c1a9b513f1998775cd00e9c
ben1289 <ben_1289@163.com>
2024-04-15 10:29:56 +0800

    refactor(layout): 面包屑增加图标

--- src/layout/components/Breadcrumb/src/Breadcrumb.vue
+++ src/layout/components/Breadcrumb/src/Breadcrumb.vue
@@ -8,6 +8,7 @@ const route = useRoute()
   <ABreadcrumb>
     <template v-for="r in route.matched">
       <ABreadcrumbItem v-if="r.meta.title" :key="r.path">
+        <RuntimeIcon v-if="r.meta.icon" :name="r.meta.icon" />
         {{ (r.meta as any).title }}
       </ABreadcrumbItem>
     </template>
