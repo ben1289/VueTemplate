cbfb6099675731d46ee3c338fbf039e20c9dacaa
ben1289 <ben_1289@163.com>
2024-07-09 11:20:15 +0800

    refactor(components): 使 QueryForm 根据自身宽度调整自适应

--- src/components/QueryForm/src/QueryForm.vue
+++ src/components/QueryForm/src/QueryForm.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import type { FormInstance } from '@arco-design/web-vue'
+import type { MaybeComputedElementRef } from '@vueuse/core'
 import type { QueryFormProps } from './QueryForm'
-import { useWindowSize } from '@vueuse/core'
+import { useElementSize } from '@vueuse/core'
 
 defineOptions({ name: 'QueryForm' })
 
@@ -13,21 +14,22 @@ const emit = defineEmits<{
 }>()
 
 const { t } = useI18n()
-const { width: windowWidth } = useWindowSize()
 
 const formRef = ref<FormInstance>()
+const { width: formWidth } = useElementSize(formRef as MaybeComputedElementRef)
+const breakpoints = [
+  { min: 1536, cols: 5 },
+  { min: 1024, cols: 4 },
+  { min: 768, cols: 3 },
+  { min: 512, cols: 2 },
+  { min: 0, cols: 1 },
+]
+const cols = computed(() => breakpoints.find(bp => formWidth.value >= bp.min)!.cols)
 const showMore = ref(false)
 const schemas = computed(() => {
-  const width = toValue(windowWidth)
-  const breakpoints = [
-    { min: 1536, cols: 5 },
-    { min: 1024, cols: 4 },
-    { min: 768, cols: 3 },
-    { min: 0, cols: 2 },
-  ]
-  const cols = breakpoints.find(bp => width >= bp.min)!.cols
+  const _cols = toValue(cols)
   // 不显示更多时，去除最后一列用于显示按钮
-  const showCols = toValue(showMore) ? cols : cols - 1
+  const showCols = toValue(showMore) || _cols === 1 ? _cols : _cols - 1
   return [props.schema.slice(0, showCols), props.schema.slice(showCols)]
 })
 
@@ -38,12 +40,7 @@ function reset() {
 </script>
 
 <template>
-  <AForm
-    ref="formRef"
-    class="grid-cols-1 gap-x-16px gap-y-8px !grid 2xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2"
-    :model="model"
-    layout="inline"
-  >
+  <AForm ref="formRef" :model="model" layout="inline">
     <template v-for="(_schema, i) in schemas">
       <template v-if="i !== 1 || showMore">
         <AFormItem v-for="item in _schema" :key="item.field" :label="item.label" :field="item.field" v-bind="item.formItemAttrs">
@@ -75,6 +72,12 @@ function reset() {
 </template>
 
 <style scoped lang="less">
+.arco-form {
+  display: grid;
+  gap: 8px 16px;
+  grid-template-columns: repeat(v-bind(cols), 1fr);
+}
+
 .arco-form-item {
   margin: 0;
 }
